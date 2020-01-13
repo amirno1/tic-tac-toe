2801c0d309e7d58659291e7b8ed2a0975880cd52
Amir <amir@Amirs-MacBook-Pro.local>
2020-01-13 18:06:47 +0100

    board and information made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hellow World</div>;
+  const [player, setPlayer] = useState("X");
+  const [isO, setIsO] = useState(false);
+
+  const click = e => {
+    !isO
+      ? (e.target.innerText = "X") && setPlayer("O")
+      : (e.target.innerText = "O") && setPlayer("X");
+    setIsO(!isO);
+  };
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+        <div className="section" onClick={click}></div>
+      </div>
+      <div className="information">Next player : {player}</div>
+    </div>
+  );
 }
 
 export default App;
